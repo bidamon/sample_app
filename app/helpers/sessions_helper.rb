92addc3cebb9c70e9f61e5009ecabaaba91b1e72
biham <bidamon@gmail.com>
2016-06-21 22:36:21 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,6 +25,13 @@ module SessionsHelper
     end
   end
   
+  
+  # Returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
+  
+  
   # Returns true if user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
@@ -43,5 +50,17 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end 
+  
+  # Redirests to stores location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+  
   
 end
